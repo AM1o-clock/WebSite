8beee2c002bbb55a33028187021949081cf5c159
AM1o-clock <y1c2m2i0@gmail.com>
2024-10-29 10:37:18 +0900

    初稿

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,30 +1,47 @@
+// ハンバーガーメニュー
 var hamburger = $('.hamburger__menu');
-
-// OPEN/CLOSEボタンをクリックしたら
-$('.hamburger__button').on('click', function() {
-  // console.log('click');
-
-  // .hamburgerの表示・非表示を繰り返す
+$('.hamburger__button').on('click', function () {
   hamburger.toggleClass('hamburger__menu--active');
-
 });
 
-// 画面幅のサイズが変わったら
-$(window).on('resize', function() {
-  // console.log('resize');
-
-  // ハンバーガーメニューを閉じる
+$(window).on('resize', function () {
   hamburger.removeClass('hamburger__menu--active');
 });
 
 
-// アコーディオンのタイトルがクリックされたら
-$('.accordion-title').on('click', function(e) {
 
-  // .accordion-contentを選択
-  var content = $(this).next();
+// アコーディオン
+$(function () {
+  $('.accordion__item--answer').hide();
+  $('.accordion__item--question').on('click', function () {
+    $(this).toggleClass("open");
+    $(this).next().slideToggle();
+    $(this).parent().toggleClass("open");
 
-  // .accordion-contentを表示・非表示
-  content.slideToggle();
+  });
+});
+
+
+// カルーセル
+const mySwiper = new Swiper('.swiper', {
+  slidesPerView: 1, // コンテナ内に表示させるスライド数（CSSでサイズ指定する場合は 'auto'）
+  centeredSlides: true, // アクティブなスライドを中央に配置する
+  speed: 500, // スライドアニメーションのスピード（ミリ秒）
+
+  autoplay: { // 自動再生させる
+    delay: 3000, // 次のスライドに切り替わるまでの時間（ミリ秒）
+    disableOnInteraction: false, // ユーザーが操作しても自動再生を止めない
+    waitForTransition: false, // アニメーションの間も自動再生を止めない（最初のスライドの表示時間を揃えたいときに）
+  },
+
+  grabCursor: true, // PCでマウスカーソルを「掴む」マークにする
+  watchSlidesProgress: true, // スライドの進行状況を監視する
+
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+    clickable: "clickable",
+  },
 
 });
+
